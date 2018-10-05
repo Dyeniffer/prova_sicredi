cf0cdb1db11374e77902252c4a5f177a015a0c63
Dyeniffer <dyeniffer_souza@hotmail.com>
2018-10-05 11:22:10 -0300

    Ajustes de identação.

--- src/desafio1.java
+++ src/desafio1.java
@@ -8,92 +8,96 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class UntitledTestCase {
-private WebDriver driver;
-private String baseUrl;
-private boolean acceptNextAlert = true;
-private StringBuffer verificationErrors = new StringBuffer();
+    private WebDriver driver;
+    private String baseUrl;
+    private boolean acceptNextAlert = true;
+    private StringBuffer verificationErrors = new StringBuffer();
 
-@Before
-public void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    baseUrl = "https://www.grocerycrud.com/demo/bootstrap_theme";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-}
-
-// Preencher os campos do formulário e salvar
-@Test
-public void testUntitledTestCase() throws Exception {
-    driver.get("https://www.grocerycrud.com/demo/bootstrap_theme");
-    driver.findElement(By.id("switch-version-select")).click();
-    new Select(driver.findElement(By.id("switch-version-select"))).selectByVisibleText("Bootstrap V4 Theme");
-    driver.findElement(By.id("switch-version-select")).click();
-    driver.findElement(By.linkText("Add Customer")).click();
-    driver.findElement(By.id("field-customerName")).click();
-    driver.findElement(By.id("field-customerName")).sendKeys("Teste Sicredi");
-    driver.findElement(By.id("field-contactLastName")).click();
-    driver.findElement(By.id("field-contactLastName")).sendKeys("Teste");
-    driver.findElement(By.id("field-contactFirstName")).click();
-    driver.findElement(By.id("field-contactFirstName")).sendKeys("Dyeniffer");
-    driver.findElement(By.id("field-phone")).click();
-    driver.findElement(By.id("field-phone")).sendKeys("51 9999-9999");
-    driver.findElement(By.id("field-addressLine1")).click();
-    driver.findElement(By.id("field-addressLine1")).sendKeys("Av Assis Brasil,3970");
-    driver.findElement(By.id("field-addressLine2")).click();
-    driver.findElement(By.id("field-addressLine2")).sendKeys("Torre D");
-    driver.findElement(By.id("field-city")).click();
-    driver.findElement(By.id("field-city")).sendKeys("Porto Alegre");
-    driver.findElement(By.id("field-state")).click();
-    driver.findElement(By.id("field-state")).sendKeys("RS");
-    driver.findElement(By.id("field-postalCode")).click();
-    driver.findElement(By.id("field-postalCode")).sendKeys("91000-000");
-    driver.findElement(By.id("field-country")).click();
-    driver.findElement(By.id("field-country")).sendKeys("Brasil");
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='from Employeer'])[1]/following::span[1]")).click();
-    driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Firrelli'])[4]/following::li[1]")).click();
-    driver.findElement(By.id("field-creditLimit")).click();
-    driver.findElement(By.id("field-creditLimit")).sendKeys("200");
-    driver.findElement(By.id("form-button-save")).click();
-}
-
-@After
-public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-    fail(verificationErrorString);
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        baseUrl = "https://www.grocerycrud.com/demo/bootstrap_theme";
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
-}
 
-private boolean isElementPresent(By by) {
-    try {
-    driver.findElement(by);
-    return true;
-    } catch (NoSuchElementException e) {
-    return false;
+    // Preencher os campos do formulário e salvar
+    @Test
+    public void testUntitledTestCase() throws Exception {
+        driver.get("https://www.grocerycrud.com/demo/bootstrap_theme");
+        driver.findElement(By.id("switch-version-select")).click();
+        new Select(driver.findElement(By.id("switch-version-select"))).selectByVisibleText("Bootstrap V4 Theme");
+        driver.findElement(By.id("switch-version-select")).click();
+        driver.findElement(By.linkText("Add Customer")).click();
+        driver.findElement(By.id("field-customerName")).click();
+        driver.findElement(By.id("field-customerName")).sendKeys("Teste Sicredi");
+        driver.findElement(By.id("field-contactLastName")).click();
+        driver.findElement(By.id("field-contactLastName")).sendKeys("Teste");
+        driver.findElement(By.id("field-contactFirstName")).click();
+        driver.findElement(By.id("field-contactFirstName")).sendKeys("Dyeniffer");
+        driver.findElement(By.id("field-phone")).click();
+        driver.findElement(By.id("field-phone")).sendKeys("51 9999-9999");
+        driver.findElement(By.id("field-addressLine1")).click();
+        driver.findElement(By.id("field-addressLine1")).sendKeys("Av Assis Brasil,3970");
+        driver.findElement(By.id("field-addressLine2")).click();
+        driver.findElement(By.id("field-addressLine2")).sendKeys("Torre D");
+        driver.findElement(By.id("field-city")).click();
+        driver.findElement(By.id("field-city")).sendKeys("Porto Alegre");
+        driver.findElement(By.id("field-state")).click();
+        driver.findElement(By.id("field-state")).sendKeys("RS");
+        driver.findElement(By.id("field-postalCode")).click();
+        driver.findElement(By.id("field-postalCode")).sendKeys("91000-000");
+        driver.findElement(By.id("field-country")).click();
+        driver.findElement(By.id("field-country")).sendKeys("Brasil");
+        driver.findElement(By
+                .xpath("(.//*[normalize-space(text()) and normalize-space(.)='from Employeer'])[1]/following::span[1]"))
+                .click();
+        driver.findElement(
+                By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Firrelli'])[4]/following::li[1]"))
+                .click();
+        driver.findElement(By.id("field-creditLimit")).click();
+        driver.findElement(By.id("field-creditLimit")).sendKeys("200");
+        driver.findElement(By.id("form-button-save")).click();
     }
-}
 
-private boolean isAlertPresent() {
-    try {
-    driver.switchTo().alert();
-    return true;
-    } catch (NoAlertPresentException e) {
-    return false;
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
     }
-}
 
-private String closeAlertAndGetItsText() {
-    try {
-    Alert alert = driver.switchTo().alert();
-    String alertText = alert.getText();
-    if (acceptNextAlert) {
-        alert.accept();
-    } else {
-        alert.dismiss();
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
     }
-    return alertText;
-    } finally {
-    acceptNextAlert = true;
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
     }
-}
+
+    private String closeAlertAndGetItsText() {
+        try {
+            Alert alert = driver.switchTo().alert();
+            String alertText = alert.getText();
+            if (acceptNextAlert) {
+                alert.accept();
+            } else {
+                alert.dismiss();
+            }
+            return alertText;
+        } finally {
+            acceptNextAlert = true;
+        }
+    }
 }
